50f8deee77e648aa885a4be68f2678f698c3496f
aavila320 <a.avila320@gmail.com>
2015-02-05 13:06:32 -0500

    adding folder

---
+++ Project 1/Quadtree/Object.cpp
@@ -0,0 +1,8 @@
+#include "Object.h"
+
+
+Object::Object( float objx, float objy, float objwidth, float objheight ) :
+	x		( objx ),
+	y		( objy ),
+	width	( objwidth ),
+	height	( objheight )
---
+++ Project 1/Quadtree/main.cpp
@@ -0,0 +1,14 @@
+#include "Quadtree.h"
+#include "Object.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+	}
+
+	return 0;
+}
